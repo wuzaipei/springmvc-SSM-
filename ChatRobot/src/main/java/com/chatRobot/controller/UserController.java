1b4f44fda31b7fa7e5ca8b89655ba66e89f69b4e
wuzaipei <41782125+wuzaipei@users.noreply.github.com>
2019-03-25 09:57:44 +0800

    Add files via upload

---
+++ ChatRobot/src/main/java/com/chatRobot/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.chatRobot.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.chatRobot.model.User;
+import com.chatRobot.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    /**
+     * Demo class
+     *
+     * @author wuzaipei
+     * @date 2016/10/31
+     */
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/showUser.do")
+    public void selectUser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        long userId = Long.parseLong(request.getParameter("id"));
+        System.out.println(userId);
+        User user = this.userService.selectUser(userId);
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(user));
+        response.getWriter().close();
+    }
+
+}
